f0b90daa7d159f71031ad00fa19ea6144d62dfe8
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:35:56 +0400

    ex3.py

---
+++ Lesson8/ex3.py
@@ -0,0 +1,6 @@
+#Write a Python function that takes two sets as input and returns a new set containing elements that are present in the first set but not in the second set.
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+difference = set1 - set2
+print(difference)
