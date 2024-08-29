16f592399554b1a284452f2ed4de14baa6d61f6b
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 18:59:48 +0400

    ex5.py

---
+++ Lesson_21/ex5.py
@@ -0,0 +1,9 @@
+#Write a function that calculates the square root of a number using the math module.
+
+import math
+
+def calculate_square_root(number):
+    return math.sqrt(number)
+
+number = 25
+print(f"The square root of {number} is {calculate_square_root(number)}")
