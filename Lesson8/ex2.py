4aebdb118e140d42ab156ad964040f3117041703
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:35:35 +0400

    ex2.py

---
+++ Lesson8/ex2.py
@@ -0,0 +1,6 @@
+#Write a Python function that takes two sets as input and returns a new set containing all unique elements from both input sets.
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+union = set1 | set2
+print(union)
