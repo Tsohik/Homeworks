c82f8b38fc471f06f2560a1234ef58455002ae34
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:43:52 +0400

    ex2.py

---
+++ Lesson9/ex2.py
@@ -0,0 +1,6 @@
+#Print a square pattern using a nested for loop. The pattern should have 5 rows and 5 columns.
+size = 5
+for i in range(size):
+    for j in range(size):
+       print("* ", end="")
+    print()
