0dda35822aba741ba09383e385361dda130d13a5
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 13:09:38 +0400

    Add files via upload

---
+++ Lesson1/ex2.py
@@ -0,0 +1,10 @@
+#Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged.
+
+homework2 = "run"
+print(len(homework2))
+print(homework2 [:3] +"ing")
+
+
+homework3 = "bad"
+print(len(homework3))
+print (homework3 [:3] + "ly")
