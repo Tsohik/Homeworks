0599e051f0c63670b323c1085d53f55221e0a3dd
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 13:28:58 +0400

    Add files via upload

---
+++ Lesson1/ex6.py
@@ -0,0 +1,7 @@
+# Write a Python function to get a string made of its first three characters of a specified string. If the length of the string is less than 3 then return the original string.
+
+homework6="Gayane"
+print(homework6 [:3])
+
+homework7="Ani"
+print(homework7[:3])
