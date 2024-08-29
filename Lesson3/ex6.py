c7626f857b2ae8126b09ebd29ee706e5c1b58887
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:08:09 +0400

    ex6.py

---
+++ Lesson3/ex6.py
@@ -0,0 +1,7 @@
+#Turn every item of a list into its square
+
+numbers = [5, 7, 8, 9, 12]
+squared_numbers = []
+for number in numbers:
+    squared_numbers.append(number ** 2)
+print("The squared numbers are:", squared_numbers)
