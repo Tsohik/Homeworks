f0d7b364a6725d0747fe451889781bdecb589e17
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 14:48:35 +0400

    ex1.py

--- Lesson2/ex1.py
+++ Lesson2/ex1.py
@@ -1,1 +1,11 @@
+#Input two integers and print out their sum. Preserve the exact format from the examples (e.g. the output should contain exactly three lines)
 
+numb1= 5
+numb2= 10
+result= numb1+numb2
+print(result)
+
+x= 256
+y= 546
+result= x+y
+print(result)
