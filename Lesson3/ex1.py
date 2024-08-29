468e54f508d75255a45a58cc459e3092637a85e3
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:06:11 +0400

    ex1.py

---
+++ Lesson3/ex1.py
@@ -0,0 +1,7 @@
+#Write a Python program to multiply all the items in a list.
+
+numbers = [1,2,3,4,]
+result = 1
+for number in numbers:
+    result *= number
+print("The result of multiplying all numbers in the list is:", result)
