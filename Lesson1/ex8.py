b4ecccf437762671b1e1a8f9b61eed264a3ae9dc
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 13:39:26 +0400

    Add files via upload

---
+++ Lesson1/ex8.py
@@ -0,0 +1,7 @@
+#Create a string made of the first, middle and last character of given string with odd number of symbols
+
+homework8="Armine"
+first=homework8[0]
+middle=homework8[2]
+last=homework8[-1]
+print(first+middle+last)
