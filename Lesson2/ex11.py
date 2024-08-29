0402ebf9dd0511315b29fcf600fb7a77e9e35253
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:03:54 +0400

    ex11.py

---
+++ Lesson2/ex11.py
@@ -0,0 +1,13 @@
+#Given a real number, round it to the nearest whole.
+
+num = 5.1
+rounded_number = round(num)
+print(rounded_number)
+
+num1 = 1.5
+rounded_number = round(num1)
+print(rounded_number)
+
+num3 = 0.2
+rounded_number = round(num3)
+print(rounded_number)
