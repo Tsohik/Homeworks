b6964d3172f09332cfdf90066658475f6f341d87
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 13:28:13 +0400

    Add files via upload

---
+++ Lesson1/ex4py.py
@@ -0,0 +1,7 @@
+# Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
+
+homework4="Karen"
+print(homework4[-1] + homework4 [1:-1] + homework4[0])
+
+homework5 ="Retin"
+print(homework5[-1]+ homework5 [1:-1] +homework5 [0])
