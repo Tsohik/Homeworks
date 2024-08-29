59242693a2938f71900196274e9eb74d0a7960b1
Tsoghik <asaturyantsoghik@gmail.com>
2024-08-29 15:03:26 +0400

    ex10.py

---
+++ Lesson2/ex10.py
@@ -0,0 +1,18 @@
+#Given the salaries of three employees working at a department, find the amount of money by which the salary of the highest-paid employee differs from the salary of the lowest-paid employee. The input consists of three positive integers - the salaries of the employees. Output a single number, the difference between the top and the bottom salaries
+
+salary1=500
+salary2=100
+salary3=1000
+max_salary= max(salary1,salary2,salary3)
+min_salary= min(salary1,salary2,salary3)
+difference=max_salary-min_salary
+print(difference)
+
+
+salary1=11
+salary2=36
+salary3=20
+max_salary= max(salary1,salary2,salary3)
+min_salary= min(salary1,salary2,salary3)
+difference=max_salary-min_salary
+print(difference)
